c0447445ec7240323c536c3c8357c0d99b4381e0
Henrik <hsn95@live.com>
2019-12-05 22:48:27 +0100

    Fixed the game-button setup

--- TouhouLauncher/ViewModel/MainViewModel.cs
+++ TouhouLauncher/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Input;
@@ -7,21 +8,42 @@ using System.Windows.Input;
 namespace TouhouLauncher.ViewModel {
 	public class MainViewModel : ViewModelBase {
 		public List<CategoryButton> CategoryList { get; }
+		public List<GameButton> GameList { get; }
 		public MainViewModel() {
 			CategoryList = new List<CategoryButton>();
-			CategoryList.Add(new CategoryButton("Main Games\n(PC 98)", new RelayCommand(() => { Debug.WriteLine("Test :P"); })));
-			CategoryList.Add(new CategoryButton("Main Games\n(Windows)", new RelayCommand(() => { Debug.WriteLine("Test :P"); })));
-			CategoryList.Add(new CategoryButton("Fighting Games", new RelayCommand(() => { Debug.WriteLine("Test :P"); })));
-			CategoryList.Add(new CategoryButton("Spin-offs", new RelayCommand(() => { Debug.WriteLine("Test :P"); })));
-			//CategoryList.Add(new CategoryButton("Fan Games", new RelayCommand(() => { Debug.WriteLine("Test :P"); })));
+			CategoryList.Add(new CategoryButton("Main Games\n(PC 98)", Test));
+			CategoryList.Add(new CategoryButton("Main Games\n(Windows)", Test));
+			CategoryList.Add(new CategoryButton("Fighting Games", Test));
+			CategoryList.Add(new CategoryButton("Spin-offs", Test));
+			CategoryList.Add(new CategoryButton("Fan Games", Test));
+			GameList = new List<GameButton>();
+			GameList.Add(new GameButton("Touhou 01"));
+			GameList.Add(new GameButton("Touhou 02"));
+			GameList.Add(new GameButton("Touhou 03"));
+			GameList.Add(new GameButton("Touhou 04"));
+			GameList.Add(new GameButton("Touhou 05"));
+			GameList.Add(new GameButton("Touhou 06"));
+			GameList.Add(new GameButton("Touhou 07"));
+			GameList.Add(new GameButton("Touhou 08"));
+			GameList.Add(new GameButton("Touhou 09"));
+			GameList.Add(new GameButton("Touhou 10"));
 		}
 		public class CategoryButton {
 			public string CategoryName { get; set; }
 			public ICommand CategoryCommand { get; set; }
-			public CategoryButton(string categoryName, ICommand categoryCommand) {
-				CategoryName = categoryName;
-				CategoryCommand = categoryCommand;
+			public CategoryButton(string name, Action action) {
+				CategoryName = name;
+				CategoryCommand = new RelayCommand(action);
 			}
+		}
+		public class GameButton {
+			public string GameName { get; set; }
+			public GameButton(string gameName) {
+				GameName = gameName;
+			}
+		}
+		private void Test() {
+			Debug.WriteLine("Test :P");
 		}
 	}
 }
