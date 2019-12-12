df43b3c137f266335e7eb4c5f61fad80d2aba72c
Henrik <hsn95@live.com>
2019-12-12 06:17:51 +0100

    Update on UI and started on code for launching games

---
+++ TouhouLauncher/Model/GameModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace TouhouLauncher.Model {
+	class GameModel {
+		public void StartGame(string gameName) {
+			Process test = new Process();
+			test.StartInfo.FileName = gameName;
+			test.Start();
+		}
+	}
+}
--- TouhouLauncher/ViewModel/MainViewModel.cs
+++ TouhouLauncher/ViewModel/MainViewModel.cs
@@ -1,54 +1,55 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using TouhouLauncher.Model;
 
 namespace TouhouLauncher.ViewModel {
 	public class MainViewModel : ViewModelBase {
-		public List<CategoryButton> CategoryList { get; }
-		public List<GameButton> GameList { get; }
+		public ObservableCollection<CategoryButton> CategoryList { get; }
+		public string CategoryPadding { get { return "10, 0"; } }
+		public ObservableCollection<GameButton> GameList { get; }
 
-		private CategoryButton _fanGameCategoryButton;
+		private GameModel _gameModel;
 		public MainViewModel() {
-			CategoryList = new List<CategoryButton> {
-				new CategoryButton("Main Games\n(PC 98)", Test),
-				new CategoryButton("Main Games\n(Windows)", Test),
-				new CategoryButton("Fighting Games", Test),
-				new CategoryButton("Spin-offs", Test)
+			_gameModel = new GameModel();
+			CategoryList = new ObservableCollection<CategoryButton> {
+				new CategoryButton("Main Games\n(PC-98)", "#342E30", SetCategory),
+				new CategoryButton("Main Games\n(Windows)", "#342E30", SetCategory),
+				new CategoryButton("Fighting Games", "#342E30", SetCategory),
+				new CategoryButton("Spin-offs", "#342E30", SetCategory),
+				new CategoryButton("Fan games", "#342E30", SetCategory),
+				new CategoryButton("Launch random\ngame", "#4284C4", SetCategory)
 			};
-			_fanGameCategoryButton = new CategoryButton("Fan Games", Test);
-			GameList = new List<GameButton>();
-			GameList.Add(new GameButton("Touhou 01"));
-			GameList.Add(new GameButton("Touhou 02"));
-			GameList.Add(new GameButton("Touhou 03"));
-			GameList.Add(new GameButton("Touhou 04"));
-			GameList.Add(new GameButton("Touhou 05"));
-			GameList.Add(new GameButton("Touhou 06"));
-			GameList.Add(new GameButton("Touhou 07"));
-			GameList.Add(new GameButton("Touhou 08"));
-			GameList.Add(new GameButton("Touhou 09"));
-			GameList.Add(new GameButton("Touhou 10"));
+			GameList = new ObservableCollection<GameButton>();
+			for (int i = 0; i < 15; i++) {
+				GameList.Add(new GameButton("Touhou " + String.Format("{0:D2}", i + 1), StartGame));
+			}
 		}
 		public class CategoryButton {
 			public string CategoryName { get; set; }
 			public ICommand CategoryCommand { get; set; }
-			public CategoryButton(string name, Action action) {
+			public string CategoryColor { get; set; }
+			public CategoryButton(string name, string colorCode, Action action) {
 				CategoryName = name;
+				CategoryColor = colorCode;
 				CategoryCommand = new RelayCommand(action);
 			}
 		}
 		public class GameButton {
 			public string GameName { get; set; }
-			public GameButton(string gameName) {
+			public ICommand GameCommand { get; set; }
+			public GameButton(string gameName, Action action) {
 				GameName = gameName;
+				GameCommand = new RelayCommand(action);
 			}
 		}
-		private void Test() {
-			Debug.WriteLine("Test");
-			CategoryList.Add(_fanGameCategoryButton);
-			RaisePropertyChanged("");
+		private void SetCategory() {
+
+		}
+		private void StartGame() {
+			_gameModel.StartGame("D:\\Games\\Touhou\\Touhou 10 Mountain of Faith\\th10e.exe");
 		}
 	}
 }
