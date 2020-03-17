6e4f33101d34e3d8860b422bc5f10a8c5e95845b
Henrik <hsn95@live.com>
2020-03-17 02:11:44 +0100

    Continued work on the settings page

---
+++ TouhouLauncher/View/UserControls/GameLocationsSettings.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TouhouLauncher.View.UserControls {
+	/// <summary>
+	/// Interaction logic for GameLocationsSettingsControl.xaml
+	/// </summary>
+	public partial class GameLocationsSettings : UserControl {
+		public GameLocationsSettings() {
+			InitializeComponent();
+		}
+	}
+}
--- TouhouLauncher/ViewModel/SettingsViewModel.cs
+++ TouhouLauncher/ViewModel/SettingsViewModel.cs
@@ -1,16 +1,33 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System.Windows.Controls;
 using System.Windows.Input;
 using TouhouLauncher.Model;
+using TouhouLauncher.View.UserControls;
 
 namespace TouhouLauncher.ViewModel {
 	public class SettingsViewModel : ViewModelBase {
+
+		public UserControl DisplayedSettings {
+			get {
+				return _settingsCategoryControls[_categoryIndex];
+			}
+		}
 		public ICommand BackCommand { get; }
 
 		private readonly SettingsModel _settingsModel;
+		private readonly UserControl[] _settingsCategoryControls;
+		private int _categoryIndex;
 		public SettingsViewModel() {
 			_settingsModel = new SettingsModel();
+			_settingsCategoryControls = new UserControl[] {
+				new GameLocationsSettings()
+			};
+			_categoryIndex = 0;
 			BackCommand = new RelayCommand(_settingsModel.CloseSettings);
+		}
+		private void SetCategoryIndex(int index) {
+			_categoryIndex = index;
 		}
 	}
 }
