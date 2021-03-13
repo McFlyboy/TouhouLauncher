060f9e93f4d68fd207c063d4948d303e9ac08326
Henrik <hsn95@live.com>
2021-03-13 04:02:36 +0100

    Started making game location UI

--- TouhouLauncher/ViewModels/GameLocationsSettingsViewModel.cs
+++ TouhouLauncher/ViewModels/GameLocationsSettingsViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.ObjectModel;
 
 namespace TouhouLauncher.ViewModels {
     public class GameLocationsSettingsViewModel : ViewModelBase {
+        public GameLocationsSettingsViewModel() {
+            GameLocations = new ObservableCollection<GameLocation>();
+
+            GameLocations.Add(new GameLocation() { Name = "Test", Location = string.Empty });
+        }
+
         public ObservableCollection<GameLocation> GameLocations { get; }
 
         public class GameLocation {
+            public string Name { get; init; }
 
+            public string Location { get; set; }
         }
     }
 }
