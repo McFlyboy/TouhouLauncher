2200b8068186869c556679967e2cf71b704de952
Henrik <hsn95@live.com>
2021-03-17 00:52:30 +0100

    More work on Game locations

--- TouhouLauncher/ViewModels/GameLocationsSettingsViewModel.cs
+++ TouhouLauncher/ViewModels/GameLocationsSettingsViewModel.cs
@@ -1,20 +1,67 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using TouhouLauncher.Models.Application.Settings;
+using TouhouLauncher.Services.Application;
 
 namespace TouhouLauncher.ViewModels {
-    public class GameLocationsSettingsViewModel : ViewModelBase {
-        public GameLocationsSettingsViewModel() {
-            GameLocations = new ObservableCollection<GameLocation>();
+	public class GameLocationsSettingsViewModel : ViewModelBase {
+		private readonly FileBrowserService _fileBrowserService;
+		private readonly SettingsManager _settingsManager;
 
-            GameLocations.Add(new GameLocation() { Name = "Test", Location = string.Empty });
-        }
+		public GameLocationsSettingsViewModel(
+			FileBrowserService fileBrowserService,
+			SettingsManager settingsManager
+		) {
+			_fileBrowserService = fileBrowserService;
+			_settingsManager = settingsManager;
 
-        public ObservableCollection<GameLocation> GameLocations { get; }
+			//GameLocations = new ObservableCollection<GameLocation>() {
+			//	new(_fileBrowserService) { Name = "Game 1", Location = string.Empty },
+			//	new(_fileBrowserService) { Name = "Game 2", Location = string.Empty }
+			//};
 
-        public class GameLocation {
-            public string Name { get; init; }
+			var test = _settingsManager
+				.OfficialGames
+				.Select(
+					(game) =>
+						new GameLocation(_fileBrowserService) {
+							Name = game.FullTitle,
+							Location = game.FileLocation
+						}
+				);
+		}
 
-            public string Location { get; set; }
-        }
-    }
+		public ObservableCollection<GameLocation> GameLocations { get; }
+
+		public class GameLocation : ObservableObject {
+			private readonly FileBrowserService _fileBrowserService;
+
+			public GameLocation(FileBrowserService fileBrowserService) {
+				_fileBrowserService = fileBrowserService;
+
+				BrowseCommand = new RelayCommand(() => {
+					Location = BrowseForGame();
+					RaisePropertyChanged(nameof(Location));
+				});
+			}
+
+			public string Name { get; init; }
+
+			public string Location { get; set; }
+
+			public ICommand BrowseCommand { get; }
+
+			private string BrowseForGame() {
+				return _fileBrowserService.BrowseFiles(
+					new FileBrowserService.Filter(
+						new FileBrowserService.Filter.FileType("Executable files", "*.exe"),
+						new FileBrowserService.Filter.FileType("All files", "*.*")
+					)
+				);
+			}
+		}
+	}
 }
