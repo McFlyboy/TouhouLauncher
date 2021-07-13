940b1cbf94cbf6e51911edbb21f7d644238cd429
Henrik <hsn95@live.com>
2021-07-13 02:22:23 +0200

    Added ini file format serializer with some tests

---
+++ TouhouLauncher.Test/Models/Infrastructure/IniSerializerServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using IniParser;
+using IniParser.Model;
+using TouhouLauncher.Models.Infrastructure;
+using static TouhouLauncher.Test.CommonTestToolsAndData;
+using Xunit;
+
+namespace TouhouLauncher.Test.Models.Infrastructure {
+	public class IniSerializerServiceTest {
+		private readonly IniSerializerService _iniSerializerService = IniSerializerService.Instance;
+
+		[Fact]
+		public void Serializes_IniData_to_ini_formatted_strings() {
+			var sectionsWithEmptySection = new SectionCollection();
+			sectionsWithEmptySection.Add(new Section("Test section"));
+
+			var iniDataWithProperty = new IniData();
+			iniDataWithProperty.Global.Add(new Property("Test property", "Hello"));
+
+			var sectionsWithNonEmptySection = new SectionCollection();
+			sectionsWithNonEmptySection.Add(new Section("Test section"));
+			sectionsWithNonEmptySection["Test section"].Add(new Property("Test property", "Hello"));
+			sectionsWithNonEmptySection["Test section"].Add(new Property("Test property 2", "Goodbye"));
+
+			var resultFromNull = _iniSerializerService.Serialize(null);
+			var resultFromNew = _iniSerializerService.Serialize(new());
+			var resultFromIniWithEmptySection = _iniSerializerService.Serialize(new() { Sections = sectionsWithEmptySection });
+			var resultFromIniWithProperty = _iniSerializerService.Serialize(iniDataWithProperty);
+			var resultFromIniWithNonEmptySection = _iniSerializerService.Serialize(new() { Sections = sectionsWithNonEmptySection });
+
+			Assert.Equal(string.Empty, resultFromNull);
+			Assert.Equal(string.Empty, resultFromNew);
+			Assert.Equal("[Test section]", resultFromIniWithEmptySection);
+			Assert.Equal("Test property=Hello", resultFromIniWithProperty);
+			Assert.Equal($"[Test section]{LineBreak}Test property=Hello{LineBreak}Test property 2=Goodbye", resultFromIniWithNonEmptySection);
+		}
+
+		[Fact]
+		public void Deserializes_ini_formatted_strings_to_IniData() {
+			var emptyResult = _iniSerializerService.Deserialize("");
+			var nonsenseResult = _iniSerializerService.Deserialize("grer43t4q3g43rab b fewfe2");
+			var sectionResult = _iniSerializerService.Deserialize("[Test section]");
+			var propertyResult = _iniSerializerService.Deserialize("Test property=Hello");
+			var sectionWithPropertiesResult = _iniSerializerService.Deserialize(
+				$"[Test section]{LineBreak}Test property=Hello{LineBreak}Test property 2=Goodbye"
+			);
+
+			Assert.Empty(emptyResult.Global);
+			Assert.Empty(nonsenseResult.Global);
+			Assert.NotNull(sectionResult.Sections["Test section"]);
+			Assert.Equal("Hello", propertyResult.Global["Test property"]);
+			Assert.Equal("Goodbye", sectionWithPropertiesResult.Sections["Test section"]["Test property 2"]);
+		}
+	}
+}
---
+++ TouhouLauncher/Models/Infrastructure/IniSerializerService.cs
@@ -0,0 +1,66 @@
+﻿using IniParser;
+using System;
+
+namespace TouhouLauncher.Models.Infrastructure {
+	public class IniSerializerService {
+		private readonly IniDataFormatter _iniDataFormatter;
+		private readonly IniDataParser _iniDataParser;
+
+		private IniSerializerService() {
+			_iniDataFormatter = new();
+
+			_iniDataParser = new();
+			_iniDataParser.Configuration.SkipInvalidLines = true;
+		}
+
+		public string Serialize(IniData data) {
+			try {
+				return _iniDataFormatter.Format(
+					data,
+					new() {
+						NumSpacesBetweenKeyAndAssigment = 0,
+						NumSpacesBetweenAssigmentAndValue = 0
+					}
+				);
+			}
+			catch (Exception e) {
+				return (e is NullReferenceException || e is ArgumentOutOfRangeException)
+					? string.Empty
+					: null;
+			}
+		}
+
+		public IniData Deserialize(string ini) {
+			try {
+				return _iniDataParser.Parse(ini);
+			}
+			catch (Exception) {
+				return null;
+			}
+		}
+
+		static IniSerializerService() {
+			Instance = new();
+		}
+
+		public static IniSerializerService Instance { get; }
+	}
+
+	public abstract record Ini {
+		public IniData Data { get; init; }
+
+		public string ToIniString() {
+			return IniSerializerService.Instance.Serialize(Data);
+		}
+	}
+
+	namespace Extensions {
+		public static partial class StringExtensions {
+			public static TIni ToIniObject<TIni>(this string iniString) where TIni : Ini, new() {
+				return new TIni() {
+					Data = IniSerializerService.Instance.Deserialize(iniString)
+				};
+			}
+		}
+	}
+}
